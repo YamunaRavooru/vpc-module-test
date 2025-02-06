110385153b428ae733bf0f0841e5d6501c6da0e1
yamuna <malini.mahathi@gmail.com>
2025-02-06 13:18:13 +0530

    vpc test module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 # output "azs_info" {
 #     value= module.vpc.azs
 # }
+# output aws_route_info{
+#     value= module.vpc.rout_table_info
+# }
--- variables.tf
+++ variables.tf
@@ -28,3 +28,6 @@ variable "private_subnet_cidr" {
 variable "database_subnet_cidr" {
     default=["10.0.21.0/24","10.0.22.0/24"]
 }
+variable "is_peering_required"{
+    default = true
+}
